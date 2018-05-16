fce7f0c7bca6e918336a1ab1fb91ace0d2574679
tomaThomas <thomas.moeller32@gmail.com>
2018-05-16 16:36:31 +0200

    GPIO test

--- stepper/__init__.py
+++ stepper/__init__.py
@@ -1,4 +1,13 @@
+import gpio
+
+startPin = 22
+
 print("Init stepper")
 
 def rotate(degrees):
     print("rotating " + str(degrees) + "°")
+
+def step():
+    print("moving step")
+
+gpio.init(startPin)
---
+++ stepper/gpio.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+
+firstPin = -10
+out = [False, False, False, False]
+
+def init(startPin):
+    global pin
+    pin = startPin
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(firstPin, GPIO.OUT)
+    GPIO.setup(firstPin + 1, GPIO.OUT)
+    GPIO.setup(firstPin + 2, GPIO.OUT)
+    GPIO.setup(firstPin + 3, GPIO.OUT)
+
+def setGPIOs(a, b, c, d):
+    global out
+    out = [a, b, c, d]
+    GPIO.output(firstPin, a)
+    GPIO.output(firstPin + 1, b)
+    GPIO.output(firstPin + 2, c)
+    GPIO.output(firstPin + 3, d)
+
+def toggle(pin):
+    global out
+    out[pin] = not out[pin]
+    GPIO.output(pin + firstPin, out[pin])

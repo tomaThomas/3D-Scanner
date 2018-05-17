69c553665eb5d99a9004280901c7aae392054a69
Paul Kupper <paul.kupper@aol.de>
2018-05-17 16:56:27 +0200

    save image to web server

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ def run():
     i = 0
     while True:
         img = cam.capture_image()
-        cam.save_image('../test' + str(i) + '.png', img)
+        cam.save_image('/web-interface/test' + str(i) + '.png', img)
         stepper.step()
         time.sleep(1)
         i+=1

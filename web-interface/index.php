4166dd7b570bc4f81acc2e8bbdb435204c61eba4
tomaThomas <thomas.moeller32@gmail.com>
2018-05-17 16:45:51 +0200

    web-interface: start python

--- web-interface/index.php
+++ web-interface/index.php
@@ -3,8 +3,17 @@
     <title>3D-Scanner</title>
 </head>
 <body>
-<?php
-
-?>
+<p><a href="log.txt">Last log</a></p>
+<p>Status:
+    <?php
+    include_once 'functions.php';
+    $pid = getPid();
+    if($pid == -1) {
+        echo "Not running";
+    } else {
+        echo "Running<br />Pid: $pid";
+    }
+    ?>
+</p>
 </body>
 </html>

ce56060e5192948ccaae2b5e3c241a8e5dd7531c
Your Name <you@example.com>
2018-05-17 18:00:54 +0000

    running python with php

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/pi/3D-Scanner
-stdbuf -oL -eL python3 run.py 2> web-interface/error.txt > web-interface/log.txt &
+python3 run.py 2> web-interface/error.txt > web-interface/log.txt &
+exit $?

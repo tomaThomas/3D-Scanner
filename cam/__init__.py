ce8056fc241835d0a8769c4f3f8cb4772c647c03
Paul Kupper <paul.kupper@aol.de>
2018-05-17 17:03:58 +0200

    debug prints

--- cam/__init__.py
+++ cam/__init__.py
@@ -5,9 +5,11 @@ cap = cv2.VideoCapture(0)
 
 
 def capture_image():
+    print("Capturing image")
     ret, frame = cap.read()
     return frame
 
 
 def save_image(path, img):
+    print("Saving image " + path)
     cv2.imwrite(path, img)

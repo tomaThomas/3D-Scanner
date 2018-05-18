6c68139d12767fa1195110ce9aad3328d9475f6a
tomaThomas <thomas.moeller32@gmail.com>
2018-05-18 10:37:19 +0200

    LOCK

--- web-interface/functions.php
+++ web-interface/functions.php
@@ -3,7 +3,6 @@ function getPid() {
     if ($file = fopen("/home/pi/3D-Scanner/LOCK", "r")) {
         if(!feof($file)) {
             $pid = fgets($file);
-            echo "<br />pid: ".$pid;
             if (file_exists( "/proc/$pid" )){
                 fclose($file);
                 return $pid;

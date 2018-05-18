8d22e27863e186788bbd85d574d7bc875c93c1ab
Paul Kupper <paul.kupper@aol.de>
2018-05-18 20:31:10 +0200

    web socket server and asyncio

--- run.py
+++ run.py
@@ -1,24 +1,70 @@
 #!/usr/bin/python
 # import stepper
-import time
 import cam
 import os
+import asyncio
+import websockets
 
 
-def run():
+async def msg_receive(socket, path):
+    print("client connected")
+    async for msg in socket:
+        print("message received: {}".format(msg))
+        answer = "echo: {}!".format(msg)
+        await socket.send(answer)
+        print("sending message: {}".format(answer))
+
+
+async def main_loop():
+    print("main loop started")
+    while True:
+        try:
+            img = cam.capture_image()
+            cam.save_image('web-interface/test.png', img)
+            # stepper.step()
+            await asyncio.sleep(1)
+        except asyncio.CancelledError:
+            print("stopping main loop")
+            raise
+
+
+async def stop_event_loop():
+    loop = asyncio.get_event_loop()
+    loop.stop()
+    print("stopped event stopped")
+
+
+def request_exit():
+    for task in asyncio.Task.all_tasks():
+        task.cancel()
+    asyncio.async(stop_event_loop())
+
+
+def main():
+    # acquire lock
     file = open("LOCK", "w")
     file.write(str(os.getpid()))
     file.close()
-    x = 0
-    while x < 3:
-        img = cam.capture_image()
-        cam.save_image('web-interface/test.png', img)
-        # stepper.step()
-        time.sleep(1)
-        x += 1
 
+    print("starting event loop")
+    loop = asyncio.get_event_loop()
+
+    open_socket = websockets.serve(msg_receive, 'localhost', 8765)
+    loop.run_until_complete(open_socket)
+
+    asyncio.async(main_loop())
+
+    # TODO: implement graceful exit
+    # for sig in (signal.SIGINT, signal.SIGTERM):
+    #     loop.add_signal_handler(sig, request_exit)
+
+    loop.run_forever()
+    loop.close()
+    print("closed event loop")
+
+    # release lock
     os.remove("LOCK")
 
 
 if __name__ == "__main__":
-    run()
+    main()

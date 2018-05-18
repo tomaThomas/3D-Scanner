7d59a1e970d4b2fcef8498a0ada8c1709ac5d001
tomaThomas <thomas.moeller32@gmail.com>
2018-05-18 10:35:51 +0200

    LOCK

--- run.py
+++ run.py
@@ -2,9 +2,13 @@
 import stepper
 import time
 import cam
+import os
 
 
 def run():
+    file = open("/home/pi/3D-Scanner/LOCK","w")
+    file.write(str(os.getpid()))
+    file.close()
     x=3
     while x>0:
         x-=1
@@ -12,6 +16,7 @@ def run():
         cam.save_image('web-interface/test.png', img)
         stepper.step()
         time.sleep(1)
+    os.remove("/home/pi/3D-Scanner/LOCK")
 
 
 if __name__ == "__main__":

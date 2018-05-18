5bfcb7e8461c89fd648c5d0d154a5d04e36d7871
Paul Kupper <paul.kupper@aol.de>
2018-05-18 18:32:37 +0200

    cleanup and relative paths

--- run.py
+++ run.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
-import stepper
+# import stepper
 import time
 import cam
 import os
 
 
 def run():
-    file = open("/home/pi/3D-Scanner/LOCK","w")
+    file = open("LOCK", "w")
     file.write(str(os.getpid()))
     file.close()
-    x=3
-    while x>0:
-        x-=1
+    x = 0
+    while x < 3:
         img = cam.capture_image()
         cam.save_image('web-interface/test.png', img)
-        stepper.step()
+        # stepper.step()
         time.sleep(1)
-    os.remove("/home/pi/3D-Scanner/LOCK")
+        x += 1
+
+    os.remove("LOCK")
 
 
 if __name__ == "__main__":

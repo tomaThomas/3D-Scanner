d53bde58249453220ea60339d2128dcb3e9047fe
tomaThomas <thomas.moeller32@gmail.com>
2018-05-18 09:33:10 +0200

    No stdout/stderr buffering

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/3D-Scanner
-python3 run.py 2> web-interface/error.txt > web-interface/log.txt &
+python3 -u run.py 2> web-interface/error.txt > web-interface/log.txt &
 exit $?

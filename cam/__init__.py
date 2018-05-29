9138e6f2b6097c1578659d13c2a6bdb046c467c1
tomaThomas <thomas.moeller32@gmail.com>
2018-05-29 17:44:01 +0200

    picam

--- cam/__init__.py
+++ cam/__init__.py
@@ -1,48 +1,17 @@
-import cv2
-import base64
-import numpy
+import picamera
+import time
+import numpy as np
 
-cap = cv2.VideoCapture(0)
+width = 320  # Multiple of 32
+height = 240  # Multiple of 16
 
+# init camera
+camera = picamera.PiCamera()
+camera.resolution = (width, height)
+time.sleep(2)
 
-def get_list():
-    return numpy.array()
-
-
-def set_width(value):
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, float(value))
-
-
-def set_height(value):
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, float(value))
+image = np.empty((height, width, 3), dtype=np.uint8)  # Creates empty 3-dimensional numpy array (rows, columns, color)
 
 
-def set_brightness(value):
-    cap.set(cv2.CAP_PROP_BRIGHTNESS, float(value))
-
-
-def set_contrast(value):
-    cap.set(cv2.CAP_PROP_CONTRAST, float(value))
-
-
-def set_saturation(value):
-    cap.set(cv2.CAP_PROP_SATURATION, float(value))
-
-
-def capture_image():
-    print('Capturing image')
-    ret, frame = cap.read()
-    return frame
-
-
-def save_image(path, img):
-    print('Saving image ' + path)
-    cv2.imwrite(path, img)
-
-
-def image_encode(img):
-    if img is not None:
-        ret, buf = cv2.imencode('.png', img)
-        res = (base64.b64encode(buf).decode('ascii'))
-        return res
-    return ''
+def get_image():
+    camera.capture(image, 'rgb')  # Fills array with current picture

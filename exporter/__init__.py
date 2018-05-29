d4fba109116e8e13d8912696dae0fec97e600b18
tomaThomas <thomas.moeller32@gmail.com>
2018-05-29 11:27:49 +0200

    Exporting mesh

--- exporter/__init__.py
+++ exporter/__init__.py
@@ -1,10 +1,12 @@
 import numpy as np
+import os
 
+path = "web-interface/data/"
 point_list = np.array([])
 
 
 def create():
-    print("Create new object...")
+    print("Creating new object...")
     global point_list
     point_list = np.array([])
 
@@ -18,3 +20,11 @@ def add_row(array):
 
 def export(name):
     print("Exporting...")
+    file_pc = open(path + name + "_pc.obj", "w")
+    for point in np.nditer(point_list):
+        file_pc.write("v " + str(point[0]) + " " + str(point[1]) + " " + str(point[2]) + "\n")
+    file_pc.close()
+    print("Running meshlabserver...")
+    os.system("meshlabserver -i" + path + name + "_pc.obj -o " + path + name + "_mesh.obj -s exporter/cp2mesh.mlx -l pc2mesh_log.txt")
+    print("Export finished.")
+    return path + name + "_mesh.obj"

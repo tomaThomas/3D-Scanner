ee1ca73dc4ef63236856d9dea57b1743bfcfa8b7
tomaThomas <thomas.moeller32@gmail.com>
2018-05-30 10:47:07 +0200

    Stepper fix

--- exporter/__init__.py
+++ exporter/__init__.py
@@ -7,12 +7,14 @@ point_list = np.array([])
 
 def create():
     print("Creating new object...")
+
     global point_list
     point_list = np.array([])
 
 
 def add_row(array):
     print("Adding row...")
+
     global point_list
     point_list = np.vstack((point_list, array))
     return point_list
@@ -22,13 +24,8 @@ def export(name):
     print("Exporting...")
 
     file_pc = open(path + name + "_pc.obj", "w")
-    for ix,iy in np.ndindex(point_list.shape):
-        if iy == 0:
-            file_pc.write("v " + str(point_list[ix, iy]))
-        elif iy == 1:
-            file_pc.write(" " + str(point_list[ix, iy]))
-        else:
-            file_pc.write(" " + str(point_list[ix, iy]) + "\n")
+    for x in range(0, point_list.shape[0]):
+        file_pc.write("v " + str(point_list[x][0]) + " " + str(point_list[x][1]) + " " + str(point_list[x][2]) + "\n")
     file_pc.close()
 
     print("Running meshlabserver...")
